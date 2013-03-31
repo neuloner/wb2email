eb7114fc999ecd52dab73c4be861944f665b52be
JiandongCao <neu.loner@gmail.com>
2013-03-31 16:43:47 +0800

    deploy.sh 	init.php 	lib/smarty/ 	lib/smarty_init.php 	script/ 	smartytest.php 	static/ 	test/ 	tpls/

--- callback.php
+++ callback.php
@@ -3,9 +3,10 @@ session_start();
 
 include_once( 'config.php' );
 include_once( 'saetv2.ex.class.php' );
-
+require_once( 'init.php' );
 $o = new SaeTOAuthV2( WB_AKEY , WB_SKEY );
-
+global $db;
+$db = new Db();
 if (isset($_REQUEST['code'])) {
 	$keys = array();
 	$keys['code'] = $_REQUEST['code'];
@@ -15,23 +16,57 @@ if (isset($_REQUEST['code'])) {
 	} catch (OAuthException $e) {
 	}
 }
+$token = $_SESSION['token'];
 
-if ($token) {
+if (isset($token)) {
+    $tplData['tokenSuccess'] = 1;
 	$_SESSION['token'] = $token;
 	setcookie( 'weibojs_'.$o->client_id, http_build_query($token) );
-?>
-<form method=POST action='register.php'>
-    授权完成, 请输入您的email地址<br>
-<input name='email'/>
-<input type="submit" />
-</form>
-<?php
-} 
-
-
-else {
-?>
-授权失败。
-<?php
+    $c = new SaeTClientV2( WB_AKEY , WB_SKEY , $token['access_token'] );
+    $uid_get = $c->get_uid();
+    $uid = $uid_get['uid'];
+    $userInfo = $c->show_user_by_id($uid);
+    $tplData['screenName'] = $userInfo['screen_name'];
+    $userId = getUserId($uid);
+    if(empty($userId))
+        $tplData['hasRegister'] = 0;
+    else {
+        $tplData['hasRegister'] = 1;
+        $userId = $userId['userId'];
+        $userEmails = getUserEmail($userId);
+        $tplData['userEmails'] = $userEmails; 
+    }
+} else {
+    $tplData['tokenSuccess'] = 0;   
 }
-?>
+$smarty->assign($tplData);
+$smarty->display('index.tpl');
+
+function getUserId($uid) {
+    global $db;
+    $sql = "select userid from users where wbid=$uid"; 
+    $res = $db->query($sql);
+    $row = mysqli_fetch_row($res);
+    if(empty($row))
+        return array();
+    else return array('userId' => $row[0]);
+}
+
+function getUserEmail($userId) {
+    $arrEmail = array();
+    global $db;
+    $sql = "select email,lasttime from useremail where userid=$userId order by id desc";
+    $res = $db->query($sql);
+    while($row = mysqli_fetch_row($res)) {
+        if($row[1] > 0) {
+            $lastTime = date("Y-m-d h:i:s A",$row[1]); 
+        } else {
+            $lastTime = '';
+        }
+        $arrEmail[] = array(
+            'email' => $row[0],
+            'lastTime' => $lastTime,
+        );
+    }
+    return $arrEmail;
+}
